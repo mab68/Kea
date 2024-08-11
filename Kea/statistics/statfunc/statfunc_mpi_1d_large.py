18cb569ec4c3763f98b3e0004fb14a6efe5ba6d1
mab68 <mark@bishop.kiwi.nz>
2024-08-12 08:26:57 +1200

    scaling, b debate

--- Kea/statistics/statfunc/statfunc_mpi_1d_large.py
+++ Kea/statistics/statfunc/statfunc_mpi_1d_large.py
@@ -12,39 +12,33 @@ except:
 from .. import moments
 
 
-def process_lags(ar, lags, periodic):
+def process_lags(ar, lags):
     out = np.zeros(len(lags))
     N = len(ar)
     for i, l in enumerate(lags):
-        if periodic:
-            X = ar[:]
-            Xp = np.roll(ar[:], l, axis=0)
-        else:
-            X = ar[l:]
-            Xp = ar[:N-l]
-        out[i] = np.nanmean(np.abs(X - Xp)**2)
+        out[i] = np.nanmean(np.abs(ar[l:] - ar[:N-l])**2)
+    return out
+
+def process_lags_per(ar, lags):
+    out = np.zeros(len(lags))
+    N = len(ar)
+    for i, l in enumerate(lags):
+        out[i] = np.nanmean(np.abs(ar - np.roll(ar, l, axis=0))**2)
     return out
 
 def mpi_pool_lagvecs(ar, max_lag, periodic):
-    lags = np.arange(0, max_lag, 1)
+    p_func = process_lags
+    if periodic:
+        p_func = process_lags_per
     # Not running in mpi, so just compute the statfunc
     if (comm is None) or comm.Get_size() == 1:
-        return lags, process_lags(ar, lags, periodic)
-    if comm.Get_rank() == 0:
-        split_lags = np.array_split(lags, comm.Get_size())
-    else:
-        split_lags = None
-    # Tell each process what lags they should calculate
-    split_shifts = comm.scatter(split_lags, root=0)
-    print('starting %s:%s' % (comm.Get_rank(), len(split_shifts)))
+        lags = np.arange(0, max_lag, 1)
+        return lags, p_func(ar, lags)
+    # Compute the structure function without any communication whatsoever
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+    lags = np.arange(rank, max_lag, size)
+    print('starting %s:%s' % (comm.Get_rank(), len(lags)))
     # Compute lag function
-    statfunc = process_lags(ar, split_shifts, periodic)
-    # Wait until everyone has finished processing
-    comm.Barrier()
-    # Retrieve and combine functions
-    rcv = np.zeros(len(lags))
-    comm.Gather(statfunc, rcv, root=0)
-    if comm.Get_rank() == 0:
-        return lags, rcv
-    else:
-        return None, None
+    statfunc = p_func(ar, lags)
+    return lags, statfunc
