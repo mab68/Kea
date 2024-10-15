a46019be3b018d61bb43d35c67b853b8c6695aed
mab68 <mark@bishop.kiwi.nz>
2024-10-15 20:39:07 +1300

    Strfn rebase

--- Kea/statistics/spectra/strfn_spectra.py
+++ Kea/statistics/spectra/strfn_spectra.py
@@ -11,47 +11,65 @@ omni_spectrum\n
 
 import numpy as np
 
-from scipy.interpolate import interp1d
+import sympy as sp
 
+from ..statfunc import statfunc_mpi
 from .. import statistics_base
-from ..statfunc import statfunc_base
 
 
-def interpolate(k_sf, fek_sf, k_interp):
-    """interpolate(k_sf, fek_sf, k_interp)
+def get_powerlaw(k, fek, num_bins=16, log_space=False):
+    est_alpha = np.gradient(np.log(fek), np.log(k))
+    b_k, b_est_alpha, _ = statistics_base.bin_data(
+        k, est_alpha, bin_func=np.nanmean, log_space=log_space,
+        bin_loc='left', num_bins=num_bins, min_bin=np.nanmin(k), max_bin=np.nanmax(k),
+        ignore_nan=True)
+    est_alpha = interpolate.logx_interpolate(b_k, b_est_alpha, k)
+    return est_alpha
 
-    Interpolates the structure function spectrum to `k_interp` to match
-        the Fourier wavenumbers
-    
-    Args:
-        k_sf (np.ndarray):
-        fek_sf (np.ndarray):
-        k_interp (np.ndarray):
-    Returns:
-    """
-    f_int = interp1d(np.log10(k_sf), np.log10(fek_sf), kind='linear', fill_value='extrapolate')
-    fek_new = f_int(np.log10(k_interp))
-    return 10**fek_new
-
-
-def omni_spectrum(ell, sf2, a=1., b=1.):
-    """omni_spectrum(ell, sf2, a, b)
-    
-    Calculates the equivalent spectrum, the integrated/omni spectrum
-        from the derivative of the structure function
-
-    `a`, and `b` are scalings for the ambiguity present in the equivalent
-        wavenumber
-
-    Args:
-        ell (np.ndarray): 
-        sf2 (np.ndarray): Averaged 2nd order structure function
-    """
-
-    dS = np.gradient(sf2)
-    dell = np.gradient(ell)
-
+def convert_to_omni_spectrum(ko, ell, sf2, phys_dims, grid_dims, a, b):
+    _dx = phys_dims[0]/grid_dims[0]
+    _dk = 2.*np.pi/phys_dims[0]
+    dS = np.gradient(sf2, _dx)
+    dell = np.gradient(ell, _dx)
     fek = (ell)**2 * (dS/dell) * (1. / 2.) / a
     kk = b / ell
+    kk = kk[::-1]
+    fek = fek[::-1]
+    kmin = np.nanmin(ko)
+    kmax = np.nanmax(kk)
+    fek = fek[kk > kmin]
+    kk = kk[kk > kmin]
+    fek = interpolate.log_log_interpolate(kk, fek, ko[ko < kmax])
+    return ko[ko < kmax], fek
 
-    return kk[::-1], fek[::-1]
+def debias(est_alpha, b, D):
+    sp_beta = sp.symbols('beta', positive=True, real=True)
+    sp_D = sp.symbols('D', positive=True, real=True)
+    sp_b = sp.symbols('b', positive=True, real=True)
+    spec_bias = sp.S(2)*sp.S(2)**(-sp_beta) * sp_b**(sp_beta - sp.S(1)) * (sp.gamma(sp_D/sp.S(2)) * sp.gamma((sp.S(3) - sp_beta) / sp.S(2))/sp.gamma(sp_D/sp.S(2) + sp_beta/sp.S(2) - sp.Rational(1,2)))
+    spec_bias = spec_bias.subs({sp_D: sp.S(D), sp_b: sp.S(b)})
+    bias_factor = []
+    est_alpha[est_alpha > -1.01] = -1.01
+    est_alpha[est_alpha < -2.99] = -2.99
+    for _a in est_alpha:
+        bias_factor.append(np.float64(spec_bias.subs({sp_beta: -_a}).evalf()))
+    bias_factor = np.array(bias_factor)
+    return bias_factor
+
+def omni_spectrum(ar1, grid_dims, phys_dims, D):
+    """
+    
+    """
+    # Calculate the SF
+    l, sf = statfunc_mpi.process_lags(ar1)
+    # Calculate the magnitude wavenumbers
+    ko = 1.
+    # Lag-to-wavenumber factors
+    a = b = np.sqrt(2.*D + 2.)
+    if D == 1:
+        a = b = 1.
+    # Convert SF to angle-integrated spectrum
+    k, fek = convert_to_omni_spectrum(ko, l, sf, phys_dims, grid_dims, a, b)
+    a_fek = get_powerlaw(k, fek)
+    b_fek = debias(a_fek, b, D)
+    return k, fek, b_fek
--- Kea/statistics/statfunc/statfunc_mpi.py
+++ Kea/statistics/statfunc/statfunc_mpi.py
@@ -8,22 +8,77 @@ except:
     mpi4py = None
     comm = None
 
+def wrap_sf(orders=[2]):
+    def process_sf(ar, lags):
+        """process_sf(ar, lags)
 
-def correlation(X1, X1p):
-    return np.nanmean(X1 * X1p)
+        Calculate the structure function for the given lags and orders
+        """
+        out = np.zeros((len(lags), len(orders)))
+        N = np.shape(ar)
+        D = len(N)
+        for i, l in enumerate(lags):
+            s1, s2 = [], []
+            for d in range(1,D+1):
+                s1.append(slice(l[D-d],N[D-d]))
+                s2.append(slice(0,N[D-d]-l[D-d]))
+            for o, p in enumerate(orders):
+                out[i,o] = np.nanmean(np.abs(ar[tuple(s1)] - ar[tuple(s2)])**p)
+        return out
+    return process_sf
 
-def strfn(X1, X1p, p):
-    return np.nanmean(np.abs(X1 - X1p)**p)
+def wrap_acf():
+    def process_acf(ar, lags):
+        """process_acf(ar, lags)
 
-def process_lags(ar, lagvecs, periodic=False):
-    """
+        Calculate the correlation function for the given lags and orders
+        """
+        out = np.zeros(len(lags))
+        N = np.shape(ar)
+        D = len(N)
+        for i, l in enumerate(lags):
+            s1, s2 = [], []
+            for d in range(1,D+1):
+                s1.append(slice(l[D-d],N[D-d]))
+                s2.append(slice(0,N[D-d]-l[D-d]))
+            out[i] = np.nanmean(ar[tuple(s1)] * ar[tuple(s2)])
+        return out
+    return process_acf
+
+def get_all_lagvecs(shape):
+    """get_all_lagvecs(shape)
     
+    Get all the lagvectors as a list required to compute the positive lag quadrant of the SF
     """
-    raise NotImplementedError()
+    return np.transpose(np.indices(shape).reshape((-1, np.prod(shape))))
 
-def mpi_pool_lagvecs(ar, lagvecs, periodic=False):
-    """
-    
-    """
-    raise NotImplementedError()
+def process_lags(ar, max_lag=None, lag_func=wrap_sf()):
+    """process_lags(ar, max_lag, lag_func)
 
+    Calculates the second order SF by naively distributing the lags amongst the available
+        processors. Returns the lags, and SF computed by the current processor
+    You will need to save the outputs, and recombine into one large SF later on.
+
+    Args:
+        ar (np.ndarray): Numpy ND array structure to compute the SF for
+        max_lag (int/None): Maximum lag to go to. If None, use N//2
+        lag_func (func): Function to compute. Default is SF of order 2.
+    Returns:
+        l (np.ndarray): Lags corresponding to the SF for the current processor
+        sf (np.ndarray): SF computed by the current processor
+    """
+    if max_lag is None:
+        max_lag = np.min(np.shape(ar))//2
+    max_lag = tuple([max_lag for _ in range(ar.ndim)])
+    # Not running in mpi, so just compute the statfunc
+    if (comm is None) or comm.Get_size() == 1:
+        lags = get_all_lagvecs(max_lag)
+        return lags, lag_func(ar, lags)
+    # Compute the structure function without any communication whatsoever
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+    lags = get_all_lagvecs(max_lag)[rank::size]
+    print('starting %s:%s' % (comm.Get_rank(), len(lags)))
+    # Compute lag function
+    statfunc = lag_func(ar, lags)
+    return lags, statfunc
