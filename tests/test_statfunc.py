7839f460ae5a7431faa2529168cf7fa25039cc41
Mark Bishop <mark@bishop.kiwi.nz>
2026-06-22 20:40:34 +1200

    add tests

--- tests/test_statfunc.py
+++ tests/test_statfunc.py
@@ -8,7 +8,6 @@ import pytest
 ####################################################################
 # Make sure that we can actually generate 1,2,3 dimensional shapes #
 ####################################################################
-
 def test_p_corr_shape_1d():
     N, D = 16, 1
     FIELD_SHAPE = (N,)*D
@@ -72,3 +71,33 @@ def test_sf_shape_3d():
     assert np.shape(lvec) == ((N//2)**D,D), 'Lag vectors not expected shape'
     assert np.shape(acf) == ((N//2)**D,1), 'SF not expected shape'
 
+#######################
+# Test implementation #
+#######################
+def test_sf_increment():
+    N, D = 16, 1
+    field = np.array(np.arange(0, N))
+    lvec, sf = structure_function(field)
+    assert sf[0] == 0., 'SF did not produce the correct value'
+    assert sf[1] == 1., 'SF did not produce the correct value'
+
+def test_corr_1d():
+    VAL = 5.
+    N, D = 16, 1
+    field = VAL*np.ones((N,)*D)
+    lvec, acf = partial_correlation_function(field)
+    assert np.allclose(acf, (VAL**2)*np.ones(((N//2)**D,1))), 'ACF did not produce the correct value'
+
+def test_corr_2d():
+    VAL = 5.
+    N, D = 16, 2
+    field = VAL*np.ones((N,)*D)
+    lvec, acf = partial_correlation_function(field)
+    assert np.allclose(acf, (VAL**2)*np.ones(((N//2)**D,1))), 'ACF did not produce the correct value'
+
+def test_corr_3d():
+    VAL = 5.
+    N, D = 16, 3
+    field = VAL*np.ones((N,)*D)
+    lvec, acf = partial_correlation_function(field)
+    assert np.allclose(acf, (VAL**2)*np.ones(((N//2)**D,1))), 'ACF did not produce the correct value'
--- tests/test_utility.py
+++ tests/test_utility.py
@@ -1,2 +1,47 @@
 
+from kea.utils import fitting, functions, geometry, get_calculation_mode, set_calculation_mode, compute_config, CalculationMode
 
+import numpy as np
+
+import pytest
+
+def test_sphere_1d():
+    D = 1
+    r, db = 1000., 1.
+    r2 = r + db
+    assert np.isclose(2., geometry.volume_hypersphere(r2, D) - geometry.volume_hypersphere(r, D)), 'Incorrect shell volume'
+
+def test_sphere_2d():
+    D = 2
+    r, db = 1000., 1.
+    r2 = r + db
+    r_center = (r + r2)/2.
+    assert np.isclose(2.*np.pi*r_center, geometry.volume_hypersphere(r2, D) - geometry.volume_hypersphere(r, D)), 'Incorrect shell volume'
+
+def test_sphere_3d():
+    D = 3
+    r, db = 1000., 1.
+    r2 = r + db
+    r_center = (r + r2)/2.
+    assert np.isclose(4.*np.pi*r_center**2, geometry.volume_hypersphere(r2, D) - geometry.volume_hypersphere(r, D)), 'Incorrect shell volume'
+
+def test_compute_config():
+    set_calculation_mode(use_cpu=False, use_gpu=True)
+    assert (False, True, False) == get_calculation_mode(), 'Did not properly update compute config'
+    assert (CalculationMode.USE_GPU,) == compute_config.CALCULATION_MODE_FLAGS, 'Did not properly update compute config'
+    set_calculation_mode(use_cpu=True, use_gpu=False)
+    assert (True, False, False) == get_calculation_mode(), 'Did not properly update compute config'
+    assert (CalculationMode.USE_CPU,) == compute_config.CALCULATION_MODE_FLAGS, 'Did not properly update compute config'
+    set_calculation_mode(use_cpu=True, use_gpu=False, do_distribute=True)
+    assert (True, False, True) == get_calculation_mode(), 'Did not properly update compute config'
+    assert (CalculationMode.USE_CPU,CalculationMode.DO_DISTRIBUTE) == compute_config.CALCULATION_MODE_FLAGS, 'Did not properly update compute config'
+
+def test_physdims():
+    geometry.DEFAULT_PHYS_SCALE = 1.
+    assert geometry.DEFAULT_PHYS_SCALE == 1., 'Did not update physical scale'
+
+def test_powerlaw():
+    k = np.linspace(1., 10., 1000)
+    E = functions.pure_powerlaw(k, powerlaw=2.)
+    est_pow = fitting.get_powerlaw(k, E)
+    assert np.allclose(2.*np.ones(len(k)), est_pow), 'Did not estimate correct power-law'
